409888c19b0d0bb56a97b649a8e2a5b488709b15
Your Name <pavlo.morozenko@ukr.net>
2023-02-06 17:04:02 +0100

    01-gallery 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
-console.log(galleryItems);
+const gallerySection = document.querySelector(".gallery");
+const galleryMarkUp = createGalleryMarkUp(galleryItems);
+
+gallerySection.insertAdjacentHTML("beforeend", galleryMarkUp);
+gallerySection.addEventListener("click", onGalleryImageClick);
+
+function createGalleryMarkUp(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+            <div class="gallery__item">
+            <a class="gallery__link"
+            href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+            </a>
+            </div>`;
+    })
+    .join("");
+}
+function onGalleryImageClick(event) {
+  event.preventDefault();
+  const closeModal = (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  };
+  const instance = basicLightbox.create(
+    `
+    <img width="800" height="600" src = ${event.target.dataset.source}>`,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", closeModal);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", closeModal);
+      },
+    }
+  );
+  instance.show();
+}
