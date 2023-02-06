f93ea62b54eb60efc5a9d62b1feea92bb48505e6
Your Name <pavlo.morozenko@ukr.net>
2023-02-06 18:56:05 +0100

    02-lightbox 1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallerySection = document.querySelector(".gallery");
+const galleryMarkUp = createGalleryMarkUp(galleryItems);
+
+gallerySection.insertAdjacentHTML("beforeend", galleryMarkUp);
+gallerySection.addEventListener("click", onGalleryImageClick);
+
+function createGalleryMarkUp(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+            <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
 
 console.log(galleryItems);
